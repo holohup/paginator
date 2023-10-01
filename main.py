ae6602e850d6cee1959ca973c1f9b8d3a34a7e75
Alexey Shapiro <academ@gmail.com>
2023-10-01 12:51:09 +0300

    iteration over intervals instead of indexes - more readability and faster code!

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ class Interval(NamedTuple):
 class Paginator:
     """Main paginator class. Use method get_pages() for the pagination str."""
 
-    _open, _close = -1, 1
-    _attrs = '_current_page', '_total_pages', '_boundaries', '_around'
+    OPEN, CLOSE = -1, 1
+    _attrs = '_current_page', '_last_page', '_boundaries', '_around'
 
     def __init__(
         self, current_page: int, total_pages: int, boundaries: int, around: int
     ) -> None:
         self._current_page = current_page
-        self._total_pages = total_pages
+        self._last_page = total_pages
+        self._first_page = 1
         self._boundaries = boundaries
         self._around = around
         self._validate_values()
@@ -29,14 +30,17 @@ class Paginator:
     def get_pages(self) -> str:
         sorted_events = sorted(self._scribe_interval_events())
         sorted_intervals = self._sort_open_intervals(sorted_events)
-        return ' '.join(self._pagination_results_generator(sorted_intervals))
+        return ' '.join(self._generate_pagination_results(sorted_intervals))
 
     def _scribe_interval_events(self) -> list[tuple]:
         events = [
-            (max(1, self._current_page - self._around), self._open),
             (
-                min(self._current_page + self._around, self._total_pages),
-                self._close,
+                max(self._first_page, self._current_page - self._around),
+                self.OPEN,
+            ),
+            (
+                min(self._current_page + self._around, self._last_page),
+                self.CLOSE,
             ),
         ]
         if self._boundaries > 0:
@@ -44,23 +48,25 @@ class Paginator:
         return events
 
     def _add_boundaries_events(self, events: list[tuple]) -> None:
-        events.extend(
-            [
-                (1, self._open),
-                (min(self._boundaries, self._total_pages), self._close),
-                (max(1, self._total_pages - self._boundaries + 1), self._open),
-                (self._total_pages, self._close),
-            ]
-        )
+        boundaries_events = [
+            (self._first_page, self.OPEN),
+            (min(self._boundaries, self._last_page), self.CLOSE),
+            (
+                max(self._first_page, self._last_page - self._boundaries + 1),
+                self.OPEN,
+            ),
+            (self._last_page, self.CLOSE),
+        ]
+        events.extend(boundaries_events)
 
     def _sort_open_intervals(self, intervals: list[tuple]) -> list[Interval]:
-        open_intervals = [Interval(0, 0)]
+        open_intervals = []
         open_intervals_sum = 0
         interval_start = None
         for interval in intervals:
-            if interval[1] == self._open:
+            if interval[1] == self.OPEN:
                 open_intervals_sum += 1
-            elif interval[1] == self._close:
+            elif interval[1] == self.CLOSE:
                 open_intervals_sum -= 1
             if open_intervals_sum > 0 and interval_start is None:
                 interval_start = interval[0]
@@ -69,21 +75,18 @@ class Paginator:
                 interval_start = None
         return open_intervals
 
-    def _pagination_results_generator(self, intervals: list[Interval]):
-        for i in range(1, len(intervals)):
-            if intervals[i].start - intervals[i - 1].end > 1:
+    def _generate_pagination_results(self, intervals: list[Interval]):
+        last_end = 0
+        for i in intervals:
+            if i.start - last_end > 1:
                 yield '...'
-            yield ' '.join(
-                map(str, range(intervals[i].start, intervals[i].end + 1))
-            )
-        if intervals[-1].end < self._total_pages:
+            yield ' '.join(map(str, range(i.start, i.end + 1)))
+            last_end = i.end
+        if last_end < self._last_page:
             yield '...'
 
     def _validate_values(self) -> None:
-        if (
-            self._negative_attr_present
-            or self._current_page > self._total_pages
-        ):
+        if self._negative_attr_present or self._current_page > self._last_page:
             raise ValueError('Invalid pagination parameters values.')
         if not self._all_attrs_are_integers:
             raise TypeError('Pagination requires all integer numbers.')
